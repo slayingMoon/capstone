04366ce60bfa3d8479a66f07d0dbbcb378237601
Georgi Kolev <georgi.kolev@nevexis.com>
2023-11-30 11:36:52 +0200

    modified booking confirmation page

--- src/pages/Booking/ConfirmedBooking.js
+++ src/pages/Booking/ConfirmedBooking.js
@@ -1,12 +1,14 @@
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './ConfirmedBooking.css';
+import { Link } from "react-router-dom";
 
 const ConfirmedBooking = () => {
     return (
       <div className="container confirmed-booking">
         <FontAwesomeIcon icon={faCircleCheck} size="3x" />
-        <h2>Your reservation has been confirmed.</h2>
-        <p>You will receive an email with all the details.</p>
+        <h2>Reservation <br/>successful</h2>
+        <Link className="button-primary btn" to="/">Done</Link>
       </div>
     );
 };
